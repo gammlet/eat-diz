6c5c336f2a3489d3704c6c21b9ea87ea5d1bf016
gammlet <37348223+gammlet@users.noreply.github.com>
2025-07-26 17:29:46 +0300

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,9 @@
+python3 -m venv sim_env
+source sim_env\Scripts\activate.bat
+
+pip install matplotlib
+pip install numpy
+pip install pyinstaller
+
+pyinstaller main.py --onefile --windowed
+python3 main.py
